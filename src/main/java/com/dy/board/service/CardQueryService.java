29ea2dd907794627d8b3dbd32782a0e3b216bdbb
dyeggoSilva <dyeggo0102@live.com>
2025-03-23 00:56:00 -0300

    criação de services

---
+++ src/main/java/com/dy/board/service/CardQueryService.java
@@ -0,0 +1,18 @@
+package com.dy.board.service;
+
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class CardQueryService {
+
+    private final Connection connection;
+
+    public Optional<CardDetailsDTO> findById(final Long id) throws SQLException {
+        var dao = new CardDAO(connection);
+        return dao.findById(id);
+    }
+}
